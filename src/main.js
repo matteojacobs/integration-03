c80e269443e690a7ada8d58519f58ac7f94bfd13
jacobs_matteo <matteo.jacobs@student.howest.be>
2025-01-20 11:57:03 +0100

    code structuring

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './style.css'
+import './css/reset.css'
+import './css/style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
